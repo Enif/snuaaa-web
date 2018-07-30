30d721c7c9b79ef00c6125423bdc856e4db987dc
YOUNGJAE KIM <yjkim1803@gmail.com>
2018-07-30 22:19:57 +0900

    change folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header'
 import Aside from './components/Aside';
 import Section from './components/Section/Section';
+import Footer from './components/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-{/*         <Header /> */}
-        <Aside />
-        <Section/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header /> 
+                <div id="contents-wrapper">
+                    <Aside class="aside-left" />
+                    <Section/>
+                    <Aside class="aside-right" />
+                </div>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Root from './client/Root';
+import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
