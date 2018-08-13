e97059bbec5acc469bc20a1d9b95e144c3e7ffd1
YOUNGJAE KIM <yjkim1803@gmail.com>
2018-08-13 14:13:04 +0900

    make InputText Component

--- src/components/Login/LogInComponent.js
+++ src/components/Login/LogInComponent.js
@@ -1,22 +1,36 @@
 import React from 'react';
-
+import InputText from '../Common/InputText'
 // [TODO] Make defalt state & Make Oject contain UserInfo
 
 const LogInComponent = ({id, password, handleChange, postLogIn }) => {
     return (
         <div className="login-wrapper">
             <div className="login-input-wrapper">
-                <input type="text" className="login-input" placeholder="ID"
+                {/* <input type="text" className="login-input" placeholder="ID"
                     name="id"
                     value={id}
-                    onChange={handleChange} />
+                    onChange={handleChange} /> */}
+                {/* <form> */}
+
+                <InputText
+                    className="login-input"
+                    name="id"
+                    value={id}
+                    handleChange={handleChange}
+                    placeholder="ID"
+                    isRequired="true"
+                />
 
                 <input type="password" className="login-input" placeholder="PASSWORD"
                     name="password"
                     value={password}
-                    onChange={handleChange} />
+                    onChange={handleChange}
+                    required />
+
+                {/* <input type="submit" value="LOGIN" /> */}
 
                 <button className="login-btn" onClick={postLogIn}>LOGIN</button>
+                {/* </form> */}
                 <p>아이디 찾기 / 비밀번호 초기화</p>
             </div>      
         </div>
