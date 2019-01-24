0c8175373fc4b6f962219bcd6ff810d6c3501deb
YOUNGJAE KIM <yjkim1803@gmail.com>
2019-01-25 00:12:47 +0900

    modify login component

--- src/components/Login/LogInComponent.js
+++ src/components/Login/LogInComponent.js
@@ -1,38 +1,56 @@
 import React from 'react';
 import InputText from '../Common/InputText'
+import logo from '../../assets/img/logo.png'
 // [TODO] Make defalt state & Make Oject contain UserInfo
 
 const LogInComponent = ({id, password, handleChange, postLogIn }) => {
     return (
         <div className="login-wrapper">
-            <div className="login-input-wrapper">
+            <div className="logo-wrapper">
+                <img src={logo} />
+                <h2>LOG IN</h2>
+            </div>
+            <div className="inputs-wrapper">
+                <div className="auto-checker">
+                    <input type="checkbox" /> <p>자동 로그인</p>
+                </div>
+                <div className="login-inputs-wrapper">
+                    <div className="login-inputs">
+                        <InputText
+                            className="login-input"
+                            name="id"
+                            value={id}
+                            handleChange={handleChange}
+                            placeholder=" ID"
+                            isRequired="true"
+                        />
+                        <input type="password" className="login-input" placeholder=" PASSWORD"
+                            name="password"
+                            value={password}
+                            onChange={handleChange}
+                            onKeyDown={(e) => {if(e.keyCode == 13) postLogIn()}}
+                            required />
+                    </div>
+                    <button className="login-btn" onClick={postLogIn}>로그인</button>
+                </div>
+                <div>
+                    <button className="btn-guest">Geust</button>
+                    <p className="menu-txt">아이디 찾기 / 비밀번호 초기화</p>
+                </div>
+                
+
+                
                 {/* <input type="text" className="login-input" placeholder="ID"
                     name="id"
                     value={id}
                     onChange={handleChange} /> */}
                 {/* <form> */}
-
-                <InputText
-                    className="login-input"
-                    name="id"
-                    value={id}
-                    handleChange={handleChange}
-                    placeholder="ID"
-                    isRequired="true"
-                />
-
-                <input type="password" className="login-input" placeholder="PASSWORD"
-                    name="password"
-                    value={password}
-                    onChange={handleChange}
-                    onKeyDown={(e) => {if(e.keyCode == 13) postLogIn()}}
-                    required />
+                
 
                 {/* <input type="submit" value="LOGIN" /> */}
 
-                <button className="login-btn" onClick={postLogIn}>LOGIN</button>
+                
                 {/* </form> */}
-                <p>아이디 찾기 / 비밀번호 초기화</p>
             </div>      
         </div>
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Redirect } from 'react-router'
+import { connect } from 'react-redux'
 import SoundBox from '../../components/Home/SoundBox';
+
+const TAG = 'HOME'
 
 class Home extends React.Component {
     constructor(props){
@@ -7,45 +11,58 @@ class Home extends React.Component {
     }
 
     render() {
-        return (
-            <div id="contents-center">
-                <div id="calendar-wrapper" className="content">
-                    <div className="title-left">
-                        <h5>천문현상 &amp; 일정</h5>
-                    </div>
-                    <div className="title-right">
-                        <h5>2018년 5월</h5>
-                    </div>
-                    
-                    <table id="calendar-tb">
-                        <tbody>
-                            <tr>
-                                <th>25(Fri)</th>
-                                <th>26(Sat)</th>
-                                <th>27(Sun)</th>
-                                <th>28(Mon)</th>
-                                <th>29(Tue)</th>
-                                <th>30(Wed)</th>
-                                <th>31(Thu)</th>
-                            </tr>
-                            <tr>
-                                <td>불금</td>
-                                <td>77대 총회</td>
-                                <td>일요일</td>
-                                <td>월요병</td>
-                                <td></td>
-                                <td></td>
-                                <td>5월 안녕모임~</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
 
-                <SoundBox />
-                
-            </div>
+        const { loginState } = this.props
+        return (
+            loginState ?
+            (
+                <div id="contents-center">
+                    <div id="calendar-wrapper" className="content">
+                        <div className="title-left">
+                            <h5>천문현상 &amp; 일정</h5>
+                        </div>
+                        <div className="title-right">
+                            <h5>2018년 5월</h5>
+                        </div>
+                        
+                        <table id="calendar-tb">
+                            <tbody>
+                                <tr>
+                                    <th>25(Fri)</th>
+                                    <th>26(Sat)</th>
+                                    <th>27(Sun)</th>
+                                    <th>28(Mon)</th>
+                                    <th>29(Tue)</th>
+                                    <th>30(Wed)</th>
+                                    <th>31(Thu)</th>
+                                </tr>
+                                <tr>
+                                    <td>불금</td>
+                                    <td>77대 총회</td>
+                                    <td>일요일</td>
+                                    <td>월요병</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5월 안녕모임~</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <SoundBox />
+                </div>
+            )
+            :
+            (
+                <Redirect to='/login' />
+            )
         );
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        loginState: state.authentication.isLoggedIn
+    }
+}
+
+export default connect(mapStateToProps, undefined)(Home);
