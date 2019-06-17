9ef915de78526ef23e5eb08f30f4c141bbb6797b
YOUNGJAE KIM <yjkim1803@gmail.com>
2019-06-18 00:10:37 +0900

    bugfix soundbox

--- src/components/Home/SoundBox.js
+++ src/components/Home/SoundBox.js
@@ -4,14 +4,13 @@ import { breakLine } from 'utils/breakLine';
 
 const SoundBox = ({soundBoxInfo}) => {
 
-    let contentInfo = soundBoxInfo.content;
     return(
         <div className="soundbox-wrapper">
             <Link to ='/board/brd01'><div className="soundbox-title">NOTICE</div></Link>
             <div className="soundbox-contents-wrapper">
                 <div className="soundbox-contents">
-                    <h5>{contentInfo && contentInfo.title}</h5>
-                    <p>{breakLine(contentInfo && contentInfo.text)}</p>
+                    <h5>{soundBoxInfo && soundBoxInfo.content.title}</h5>
+                    <p>{breakLine(soundBoxInfo && soundBoxInfo.content.text)}</p>
                 </div>
             </div>
         </div>
