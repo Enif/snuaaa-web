59762821cfceaed66e23d9cdaf3e1d3259069341
YOUNGJAE KIM <yjkim1803@gmail.com>
2019-07-10 23:32:04 +0900

    apply domain address

--- src/common/environment.js
+++ src/common/environment.js
@@ -1,6 +1,6 @@
 export const SERVER_URL = function(){
     if(process.env.NODE_ENV === 'production'){
-        return 'http://52.78.161.191/'
+        return 'http://snuaaa.org/'
     }
     else if (process.env.NODE_ENV === 'development'){
         return 'http://localhost:8080/'
