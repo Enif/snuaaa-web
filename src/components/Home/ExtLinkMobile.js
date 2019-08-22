b07c127a695dd9629cec3eaa352f783651630039
YOUNGJAE KIM <yjkim1803@gmail.com>
2019-08-23 00:44:41 +0900

    add weather icon

--- src/components/Home/ExtLinkMobile.js
+++ src/components/Home/ExtLinkMobile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import icon_weather from 'assets/img/common/icon_weather.png'
 import icon_stella from 'assets/img/common/icon_stella.png';
 import icon_apod from 'assets/img/common/icon_apod.png'
 
@@ -6,10 +7,13 @@ const ExtLinkMobile = () => {
 
     return (
         <div className="ext-link-mobile">
-            <a className="link-img" href="https://stellarium-web.org/" target="_blanck">
+            <a className="link-img" href="http://www.weather.go.kr/weather/main.jsp" target="_blank">
+                <img src={icon_weather} alt="weather" />
+            </a>
+            <a className="link-img" href="https://stellarium-web.org/" target="_blank">
                 <img src={icon_stella} alt="stellarium" />
             </a>
-            <a className="link-img" href="https://apod.nasa.gov/apod/" target="_blanck">
+            <a className="link-img" href="https://apod.nasa.gov/apod/" target="_blank">
                 <img src={icon_apod} alt="apod" />
             </a>
         </div>
